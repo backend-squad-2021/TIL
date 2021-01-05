c33c8d933de8172f6ca74e7a31a9829e8d050e2c
bong6981 <kbk5190@naver.com>
2021-01-05 16:38:04 +0900

    copy code from eclipse

---
+++ cs10/bongf/Adder.java
@@ -0,0 +1,63 @@
+package cs10.bongf;
+import java.util.Arrays; 
+
+
+
+
+public class Adder {
+	// [미션]이진덧셈기 - 미션1 
+	public static boolean[] halfadder(boolean bitA, boolean bitB) {
+	        boolean sum = bitA ^ bitB;
+	        boolean carry = bitA && bitB;
+		 	boolean[] answer = {carry, sum};
+	        return answer;
+	    }
+	 
+	public static boolean[] fulladder(boolean bitA, boolean bitB, boolean carry) {
+	        boolean sum = (bitA ^ bitB) ^ carry; 
+	        boolean carry2 = (bitA && bitB) || ((bitA ^ bitB) && carry); 
+		 	boolean[] answer = {carry2, sum};
+	        return answer;
+        }
+
+    // [미션]이진덧셈기 - 미션2 
+    public static boolean[] byteadder(boolean[] byteA, boolean[] byteB) {
+            boolean[] answer = new boolean[9];
+            boolean carry = false;
+            for(int i=0; i < answer.length; i++) {
+                if(i == 8) {
+                    answer[i] = carry;
+                }else {
+                    answer[i] = (byteA[i] ^ byteB[i]) ^ carry;
+                    carry = (byteA[i] && byteB[i]) || ((byteA[i] ^ byteB[i]) && carry);
+                }
+            }
+            return answer;
+        }
+	public static void main(String[] args) {
+		// 미션 1 Test
+//		boolean bitA = true;
+//		boolean bitB = false;
+//		boolean[] answer = halfadder(bitA, bitB);
+		// 미션 2 Test
+		boolean[] byteA = new boolean[]{true, true, false, false, true, false, true, false};
+	    boolean[] byteB = new boolean[]{true, true, false, true, true, false, false, true};
+	    boolean[] answer = byteadder(byteA, byteB);
+	    int[] answer2 = new int[answer.length];
+	    for(int i=0; i<answer.length; i++) {
+	    	if(answer[i] == true) {
+	    		answer2[i] = 1;
+	    	}else {
+	    		answer2[i] = 0;
+	    	}
+	    }
+		System.out.println(Arrays.toString(answer2));
+	}
+	
+}
+
+
+
+	   
+
+    
---
+++ cs10/bongf/Convertor.java
@@ -0,0 +1,73 @@
+package cs10.bongf;
+import java.util.Arrays; 
+
+
+public class Convertor {
+	public static void main(String[] args) {
+        // 미션 1 test 
+		// boolean[] answer = dec2bin(173);
+        // System.out.println(Arrays.toString(answer));
+        // 미션 2 test
+        boolean[] bin = {true, true, true, true, false, true, false, true}; 
+		int answer = bin2dec(bin);
+		System.out.println(answer);
+		
+	}
+	//[미션]진법 변환기 - 미션1
+	public static boolean[] dec2bin(int decimal) {
+		    //배열 크기 구하기 		 
+		 	int count = 0;
+		 	int decimal2 = decimal;
+		 	while(decimal2 !=1) {
+		 		count += 1;
+		 		decimal2 = decimal2 / 2;
+		 	}
+		 	//2진수바꿔 배열로 만들기 
+		 	int[] answer_array = new int[count+1];
+		 	int i = 0; 
+		 	while(decimal != 1) {
+		 		answer_array[i++] = decimal % 2;
+		 		decimal = decimal / 2; 
+		 	}
+		 	answer_array[i] = decimal;
+		 	//boolean으로 바꾸기 
+		 	boolean[] answer = new boolean[answer_array.length];
+		 	for(int j=0; j < answer_array.length; j++) {
+		 		if(answer_array[j] == 1) {
+		 			answer[j] = true;
+		 		}else {
+		 			answer[j] = false;
+		 		}
+		 	}
+		 	return answer;
+    }
+  
+
+    //[미션]진법 변환기 - 미션2
+    public static int bin2dec(boolean[] bin) {
+		
+		int[] answer_binary = new int[bin.length];
+		for(int j=0; j < bin.length; j++) {
+			if(bin[j] == true) {
+				answer_binary[j] = 1;
+			}else {
+				answer_binary[j] = 0;
+			}		
+		}
+		int answer = 0;
+		int time = 1;  
+		for(int i=0; i < answer_binary.length; i++) {
+			if(i >= 1) {
+				time *= 2;
+				answer += answer_binary[i] * time;
+			}else {
+				answer += answer_binary[i];
+			}
+		}
+        return answer;
+    }
+}
+
+
+
+    

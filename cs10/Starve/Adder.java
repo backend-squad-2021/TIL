d8a68b270b84c6fc8a289508ab64ae7a686fe340
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-01-05 16:49:49 +0900

    Adder + Convertor Add

--- cs10/Starve/Adder.java
+++ cs10/Starve/Adder.java
@@ -2,27 +2,31 @@ public class Adder {
     public boolean[] halfAdder(boolean bitA, boolean bitB) {
         boolean carry = bitA & bitB;
         boolean sum = bitA ^ bitB;
-        boolean[] answer = {carry,sum};
+        boolean[] answer = {carry, sum};
         return answer;
     }
+
     public boolean[] fullAdder(boolean bitA, boolean bitB, boolean carry) {
-        boolean sum = (bitA ^ bitB)^carry;
-        boolean carry2 = bitA & bitB || (bitA^bitB)&carry;
-        boolean[] answer = {carry2,sum};
+        boolean[] halfAdder1 = halfAdder(bitA, bitB);
+        boolean[] halfAdder2 = halfAdder(carry, halfAdder1[1]);
+        boolean fullAdderSum = halfAdder2[1];
+        boolean fullAdderCarry = halfAdder1[0] | halfAdder2[0];
+        boolean[] answer = {fullAdderCarry, fullAdderSum};
         return answer;
     }
 
     public boolean[] byteAdder(boolean[] byteA, boolean[] byteB) {
         boolean carry = false;
-        boolean[] answer = new boolean[byteA.length+1];
-        for(int i = 0; i<byteA.length; i++){
-            boolean[] sum = fullAdder(byteA[i],byteB[i],carry);
+        boolean[] answer = new boolean[9];
+        for (int i = 0; i < answer.length - 1; i++) {
+            boolean[] sum = fullAdder(byteA[i], byteB[i], carry);
             carry = sum[0];
-            answer[i]=sum[1];
+            answer[i] = sum[1];
         }
-        answer[answer.length-1]= carry;
+        answer[answer.length - 1] = carry;
 
         return answer;
     }
 
 }
+

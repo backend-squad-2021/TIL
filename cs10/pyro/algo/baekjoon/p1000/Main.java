2c6f7424e5f588c324a671f7fb62186b2902f8cd
고정완 (Can) <jw.gho@ecubelabs.com>
2021-01-06 11:52:39 +0900

    [feat]: Solve acmicpc/1000

---
+++ cs10/pyro/algo/baekjoon/p1000/Main.java
@@ -0,0 +1,63 @@
+// https://www.acmicpc.net/problem/1000
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    private static final String lineSeparator = System.lineSeparator();
+
+    private static class Dto {
+        final int a;
+        final int b;
+
+        Dto(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public String toString() {
+            return "a=" + a + ", b=" + b;
+        }
+    }
+
+    private static String solve(Dto input) {
+        return Integer.toString(input.a + input.b);
+    }
+
+    private static void assertOutputEquals(String expected, Dto input) {
+        String output = solve(input);
+        System.out.println(lineSeparator);
+        if (expected.equals(output)) {
+            System.out.println("!Success!");
+            return;
+        }
+        System.out.println("input: " + input);
+        System.out.println("expected: " + expected);
+    }
+
+    private static void test() {
+        assertOutputEquals("3", new Dto(1, 2));
+        assertOutputEquals("12", new Dto(5, 7));
+    }
+
+    private static void main(BufferedReader br, BufferedWriter bw) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        Dto input = new Dto(a, b);
+
+        String output = solve(input);
+        bw.write(output);
+    }
+
+    public static void main(String[] args) throws IOException {
+        // test();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        main(br, bw);
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}

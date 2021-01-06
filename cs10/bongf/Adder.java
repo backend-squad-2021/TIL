c4b76dc62ec2955f94ded4fec6fc96c19de6950d
bong6981 <kbk5190@naver.com>
2021-01-06 22:54:48 +0900

    Incorporate Feedback

--- cs10/bongf/Adder.java
+++ cs10/bongf/Adder.java
@@ -1,59 +1,36 @@
 package cs10.bongf;
-import java.util.Arrays; 
-
-
-
-
 public class Adder {
-	// [미션]이진덧셈기 - 미션1 
-	public static boolean[] halfadder(boolean bitA, boolean bitB) {
+	public boolean[] halfadder(boolean bitA, boolean bitB) {
 	        boolean sum = bitA ^ bitB;
 	        boolean carry = bitA && bitB;
 		 	boolean[] answer = {carry, sum};
 	        return answer;
 	    }
 	 
-	public static boolean[] fulladder(boolean bitA, boolean bitB, boolean carry) {
-	        boolean sum = (bitA ^ bitB) ^ carry; 
-	        boolean carry2 = (bitA && bitB) || ((bitA ^ bitB) && carry); 
+	public boolean[] fulladder(boolean bitA, boolean bitB, boolean carry) {
+			boolean[] halfadder1 = halfadder(bitA, bitB);
+			boolean[] halfadder2 = halfadder(carry, halfadder1[1]);
+	        boolean sum = halfadder2[1];
+	        boolean carry2 = halfadder1[0] || halfadder2[0];
 		 	boolean[] answer = {carry2, sum};
 	        return answer;
-        }
-
-    // [미션]이진덧셈기 - 미션2 
-    public static boolean[] byteadder(boolean[] byteA, boolean[] byteB) {
-            boolean[] answer = new boolean[9];
-            boolean carry = false;
-            for(int i=0; i < answer.length; i++) {
-                if(i == 8) {
-                    answer[i] = carry;
-                }else {
-                    answer[i] = (byteA[i] ^ byteB[i]) ^ carry;
-                    carry = (byteA[i] && byteB[i]) || ((byteA[i] ^ byteB[i]) && carry);
-                }
-            }
-            return answer;
-        }
-	public static void main(String[] args) {
-		// 미션 1 Test
-//		boolean bitA = true;
-//		boolean bitB = false;
-//		boolean[] answer = halfadder(bitA, bitB);
-		// 미션 2 Test
-		boolean[] byteA = new boolean[]{true, true, false, false, true, false, true, false};
-	    boolean[] byteB = new boolean[]{true, true, false, true, true, false, false, true};
-	    boolean[] answer = byteadder(byteA, byteB);
-	    int[] answer2 = new int[answer.length];
-	    for(int i=0; i<answer.length; i++) {
-	    	if(answer[i] == true) {
-	    		answer2[i] = 1;
-	    	}else {
-	    		answer2[i] = 0;
-	    	}
 	    }
-		System.out.println(Arrays.toString(answer2));
-	}
 	
+		public boolean[] byteadder(boolean[] byteA, boolean[] byteB) {
+			boolean[] answer = new boolean[byteA.length + 1];
+			boolean carry = false;
+			
+			for(int i=0; i < answer.length; i++) {
+				if(i == 8) {
+					answer[i] = carry;
+				}else {
+					boolean[] fulladder = fulladder(byteA[i], byteB[i], carry);
+					answer[i] = fulladder[1];
+					carry = fulladder[0];
+        	}
+        }
+        return answer;
+	}	
 }
 
 

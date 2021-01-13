81bd65494f0b51d1947b77dc258c4f373a85e6fa
Kyu <59721293+kyu-kim-kr@users.noreply.github.com>
2021-01-13 18:34:55 +1300

    Update TwoSum.java
    이걸로 제출함

--- cs10/kyu/TwoSum.java
+++ cs10/kyu/TwoSum.java
@@ -1,15 +1,15 @@
 package leetcode;
 
+import java.util.Arrays;
+
 class TwoSum {
     public static int[] twoSum ( int[] nums, int target){
-       
+    
         int[] answer = new int[2];
 
-        int count = nums.length;
         for (int i = 0; i < nums.length; i++) {
-            count--;
-            for (int j = 0; j < count; j++) {
-                if (i <= j) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i >= j) {
                     continue;
                 }
                 if (target == nums[i] + nums[j]) {
@@ -23,13 +23,7 @@ class TwoSum {
         return answer;
     }
     public static void main(String[] args) {
-        System.out.println(twoSum(new int[](3, 2, 4, 6), 6);
-        /**
-         * 문제1
-         * new로 바로 인트배열만들었는데빨간줄
-         * 문제2
-         * 저렇게안하고 변수에 배열담아서 출력했는데 메모리 주소가 나옴
-         */
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 2, 4, 6}, 6)));
+    
     }
 }
-
